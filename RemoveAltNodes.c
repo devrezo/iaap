f1a6fc75eb176e48573aa2a8ef49bed6c8540c78
devrezo <devrezo@users.noreply.github.com>
2017-09-11 17:14:01 +0530

    Added:Remove alternate nodes
    Remove alternate nodes in a linked list
    check for segmentation faults in your code when accessing nextNode. i.e. nextNode = currentNode->next->next;
    so in the loop, also check against currentNode->next != NULL along with currentNode != NULL

---
+++ RemoveAltNodes.c
@@ -0,0 +1,84 @@
+/* last written on 11/09/2017 16:23:14
+owner ise2017001 rajaneesh
+@devrezo on twitter
+@devrezo on github
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct node {
+        int data;
+        struct node *next;
+};
+struct node *createNode (int value) {
+        struct node *newNode = (struct node *) malloc (sizeof (struct node));
+        newNode -> data = value;
+        newNode -> next = NULL;
+        return newNode;
+}
+void insertNode (struct node **head_ref, struct node **tail_ref, int value) {
+        struct node *newNode = createNode (value);
+        if (*head_ref == NULL) {
+                *head_ref = newNode;
+                *tail_ref = newNode;
+}
+        else {
+                (*tail_ref) -> next = newNode;
+                *tail_ref = newNode;
+}
+}
+void  printList (struct node *head) {
+        while (head != NULL) {
+                printf ("%d -> ", head->data);
+                head = head->next;
+}
+        printf ("NULL\n");
+}
+void destroyList (struct node *head) {
+        struct node *nextNode = head;
+        while (head != NULL) {
+                nextNode = head -> next;
+                free (head);
+                head = nextNode;
+}
+        printf("Killed off all her feelings\n" );
+}
+void removeAltNodes (struct node **head_ref) {
+        struct node *nextAltNode = NULL;
+        struct node *currentNode = NULL;
+        struct node *nextNode = NULL;
+        currentNode = *head_ref;
+        if (currentNode == NULL || currentNode->next == NULL) {
+                return;
+        }
+        nextNode = currentNode->next;
+        while (currentNode != NULL && currentNode->next != NULL) {
+                nextAltNode = currentNode->next;
+                nextNode = currentNode->next->next;
+                currentNode->next = nextNode;
+                free (nextAltNode);
+                currentNode = nextNode;
+        }
+        return;
+}
+int main (int argc, char *argv[]) {
+        int value;
+        struct node *head = NULL, *tail = NULL;
+        char buffer[2048];
+        char *p = NULL;
+
+        fgets (buffer, 2048, stdin);
+        p = strtok (buffer, "NULL >|\n");
+        while (p != NULL) {
+                sscanf (p, "%d", &value);
+                insertNode (&head, &tail, value);
+                p = strtok (NULL, "> | NULL \n");
+        }
+        printList (head);
+        removeAltNodes (&head);
+        printList (head);
+        destroyList (head);
+        head = NULL;
+        tail = NULL;
+        return 0;
+}
