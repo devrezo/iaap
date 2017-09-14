75ed9a523985b36a38b3cb12d9092206f35facf5
Polavarapu Rajaneesh <devrezo@users.noreply.github.com>
2017-09-14 18:30:13 +0530

    Linked list of strings separated by spaces
    User enters a big string. The string is broken down into words and the linked list is constructed with each word as a node of the list.

---
+++ LinkedListOfStrings.c
@@ -0,0 +1,58 @@
+/* last written on 13/08/2017 13:32:14
+owner ise2017001
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct node {
+        char *data;
+        struct node *next;
+};
+struct node *createNode (char *str) {
+        struct node *newNode = (struct node *) malloc (sizeof (struct node));
+        newNode -> data = str;
+        newNode -> next = NULL;
+        return newNode;
+}
+void insertNode (struct node **head_ref, struct node **tail_ref, char *str) {
+        struct node *newNode = createNode (str);
+        if (*head_ref == NULL) {
+                *head_ref = newNode;
+                *tail_ref = newNode;
+}
+        else {
+                (*tail_ref) -> next = newNode;
+                *tail_ref = newNode;
+}
+}
+void  printList (struct node *head) {
+        while (head != NULL) {
+                printf ("%s -> ", head->data);
+                head = head->next;
+}
+        printf (" NULL\n");
+}
+void destroyList (struct node *head) {
+        struct node *nextNode = head;
+        while (head != NULL) {
+                nextNode = head -> next;
+                free (head);
+                head = nextNode;
+}
+        printf("Killed off all her feelings\n" );
+}
+int main (int argc, char *argv[]) {
+        char *buffer = (char *) malloc (2048);
+        char *str = NULL;
+        struct node *head = NULL;
+        struct node *tail = NULL;
+
+        fgets (buffer, 2048, stdin);
+        str = strtok (buffer, " ");
+        while (str != NULL) {
+                insertNode (&head, &tail, str);
+                str = strtok (NULL, " \n");
+        }
+        printList (head);
+        return 0;
+}
